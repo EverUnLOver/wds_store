adbf577c6f1360d752d9d9fde350b3d56be0cddf
EverUnLOver <anewmeal@gmail.com>
2021-01-24 21:17:49 -0500

    Modificacion al modelo.

--- wds_store/shirts/models.py
+++ wds_store/shirts/models.py
@@ -3,11 +3,26 @@
 # Django
 from django.db import models
 
-class Basica(models.Model):
-    """Camisas basicas."""
+class Colore(models.Model):
+    """."""
+    color = models.CharField(max_length='15', unique=True, error_messages={'unique': 'Ya existe ese color'})
+    class Meta:
+        """."""
+        ordering = ['color']
+    def __str__(self):
+        return '%s' % (self.color)
 
-    imagen = models.ImageField(upload_to='/basicas/')
-    color = models.CharField(max_length='15', unique=True, error_messages={'unique': 'Ya esta una camisa basica con ese color.'})
+class Camisa(models.Model):
+    """Camisas basicas."""
+    CHOICE = (
+        ('HOMBRE','Hombre'),
+        ('NIÑO','Niño'),
+        ('MUJER','Mujer'),
+        ('NIÑA','Niña')
+    )
+    id = models.AutoField(primary_key=True, unique=True, error_messages={'unique': 'Ya esta esa id.'})
+    tipo = models.CharField(max_length='7', choices=CHOICE)
+    
 
     class Meta:
         """Meta class."""
@@ -15,28 +30,16 @@ class Basica(models.Model):
 
     def __str__(self):
 
-        return '%s' % (self.color)
+        return '%s' % (self.id)
 
-class Diseño(models.Model):
-    """Diseño de camisas basicas."""
-
-    nombre = models.CharField(max_length='100', unique=True, error_messages={'unique': 'Ya hay un diseño con este nonbre.'})
-    imagen = models.ImageField(upload_to='/diseños/')
-
-    class Meta:
-        """Meta class."""
-        ordering = ['nombre']
-
-    def __str__(self):
-
-        return '%s' % (self.nombre)
-
-class Basica_Diseño(models.Model):
-    """Basicas con diseño."""
-
-    id = models.AutoField(primary_key=True, unique=True, error_messages={'unique': 'Ya existe esta id.'})
-    basica = models.ImageField(upload_to='/BasicaConDiseño/')
+class Imagene(models.Model):
+    """."""
+    camisa = models.ForeignKey(Camisa, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='/camisas/')
+    color = models.ForeignKey(Colore, on_delete=models.CASCADE)
 
     class Meta:
-        """Meta class."""
-        ordering = ['id']
+        """."""
+        ordering = ['camisa', 'color']
+
+
